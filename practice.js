c4bc4207b9c5de44ac572bdfc63305548eec81eb
Blake Hagen <blakehagen@gmail.com>
2015-01-17 12:21:04 -0700

    sat 1220pm

--- practice.js
+++ practice.js
@@ -1,34 +1,36 @@
-//Once you complete a problem, open up Chrome and check the answer in the console. ok
+//Once you complete a problem, open up Chrome and check the answer in the console. 
 
 
 //Create an object called me. Give it a key of name with the value being your name, and another key of age with the value being your age. Then alert your name using dot notation.
 
   //Code here
 
-
+var me = {name: "Blake", age: 32};
+alert(me.name);
 
 
 //NEXT PROBLEM
-
-
-
 
 //Make a 'favoriteThings' object that contains the following keys: band, food, person, book, movie, holiday. Have the values to those keys be your favorite thing in that category.
 
   //Code here
 
+var favoriteThings = {band: "unknown", food: "guacamole", person: "Emily", book: "Harry Potter", movie: "Hunger Games", holiday: "Christmas"}
 
 //After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
 
   //Code here
+
+  favoriteThings.car = "Deuce";
+  favoriteThings.brand = "Nike";
 
 
 //Now change the food key in your favoriteThings object to be 'Lettuce' and change the book key in your favoriteThings object to be '50 Shades of Gray'.
 
   //Code here
 
-
-
+favoriteThings.food = "Lettuce";
+favoriteThings.book = "50 Shades of Gray";
 
 //NEXT PROBLEM
 
